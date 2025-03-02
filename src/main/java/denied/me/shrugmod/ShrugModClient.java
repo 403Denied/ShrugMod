feef6662ffaeef81ad4561d6ad8f70e1771c0a42
403Denied <66492638+403Denied@users.noreply.github.com>
2025-03-02 13:30:02 -0600

    Removed unused classes

--- src/main/java/denied/me/shrugmod/ShrugModClient.java
+++
@@ -1,8 +0,0 @@
-package denied.me.shrugmod;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class ShrugModClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {}
-}
