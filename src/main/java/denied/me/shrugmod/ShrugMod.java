7ab78191b027c1b426fae337076b27e18cda65ae
403denied <masonpetersen53@gmail.com>
2026-03-29 21:25:07 -0500

    Updated dependencies and Gradle wrapper to support new Minecraft and Fabric versions; refactored command registration to use updated API.

--- src/main/java/denied/me/shrugmod/ShrugMod.java
+++ src/main/java/denied/me/shrugmod/ShrugMod.java
@@ -3,10 +3,9 @@ package denied.me.shrugmod;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommands;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.Text;
+import net.minecraft.client.Minecraft;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,24 +17,18 @@ public class ShrugMod implements ClientModInitializer {
 	public void onInitializeClient() {
 		LOGGER.info("[ShrugMod] Registering client-side commands...");
 
-		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-			dispatcher.register(ClientCommandManager.literal("shrug")
-					.executes(ShrugMod::shrugWithoutMessage)
-					.then(ClientCommandManager.argument("message", StringArgumentType.greedyString())
-							.executes(ShrugMod::shrugWithMessage)));
-		});
-		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-			dispatcher.register(ClientCommandManager.literal("tableflip")
-					.executes(ShrugMod::tfWithoutMessage)
-					.then(ClientCommandManager.argument("message", StringArgumentType.greedyString())
-							.executes(ShrugMod::tfWithMessage)));
-		});
-		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-			dispatcher.register(ClientCommandManager.literal("unflip")
-					.executes(ShrugMod::ufWithoutMessage)
-					.then(ClientCommandManager.argument("message", StringArgumentType.greedyString())
-							.executes(ShrugMod::ufWithMessage)));
-		});
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommands.literal("shrug")
+                .executes(ShrugMod::shrugWithoutMessage)
+                .then(ClientCommands.argument("message", StringArgumentType.greedyString())
+                        .executes(ShrugMod::shrugWithMessage))));
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommands.literal("tableflip")
+                .executes(ShrugMod::tfWithoutMessage)
+                .then(ClientCommands.argument("message", StringArgumentType.greedyString())
+                        .executes(ShrugMod::tfWithMessage))));
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommands.literal("unflip")
+                .executes(ShrugMod::ufWithoutMessage)
+                .then(ClientCommands.argument("message", StringArgumentType.greedyString())
+                        .executes(ShrugMod::ufWithMessage))));
 
 	}
 
@@ -59,15 +52,15 @@ public class ShrugMod implements ClientModInitializer {
 	}
 
 	private static int sendClientChatMessage(String message) {
-		MinecraftClient client = MinecraftClient.getInstance();
+		Minecraft client = Minecraft.getInstance();
 
-		if (client.player != null && client.player.networkHandler != null) {
+		if (client.player != null) {
 			if (message.startsWith("/")) {
 				String command = message.substring(1);
-				client.player.networkHandler.sendChatCommand(command);
+				client.player.connection.sendCommand(command);
 				LOGGER.info("[ShrugMod] Executed command: /{}", command);
 			} else {
-				client.player.networkHandler.sendChatMessage(message);
+				client.player.connection.sendChat(message);
 				LOGGER.info("[ShrugMod] Sent chat message: {}", message);
 			}
 		} else {
